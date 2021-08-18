f3ff89f3fae14ea9cf676ee2746a161562e4c6e7
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-08-18 10:03:56 +0200

    Update

--- src/main/kotlin/examples/parsers/Parsers.kt
+++ src/main/kotlin/examples/parsers/Parsers.kt
@@ -1,13 +1,105 @@
 package examples.parsers
 
+import examples.parsers.Result.Failure
+import examples.parsers.Result.Success
+
 /*fun keyValuePair(input: String): Map<String, String> {
     val (key, value) = input.split(":")
     return mapOf(key to value)
 }*/
 
-fun keyValuePair(input: String): Map<String, String> {
+/*fun keyValuePair(input: String): Map<String, String> {
     return input.split("\n").map {
         val (key, value) = it.split(":")
         (key to value)
     }.toMap()
+}*/
+
+sealed class Result<out T> {
+    data class Success<T>(val match: T, var remainder: String) : Result<T>()
+    data class Failure(val expected: String, var remainder: String) : Result<Nothing>()
+
+    fun <U> map(f: (T) -> U): Result<U> = when (this) {
+        is Failure -> this
+        is Success -> Success(f(match), remainder)
+    }
+
+    fun <U> flatMap(f: (T, String) -> Result<U>): Result<U> = when (this) {
+        is Failure -> this
+        is Success -> f(match, remainder)
+    }
+
+    fun mapExpected(f: (String) -> String): Result<T> = when (this) {
+        is Success -> this
+        is Failure -> Failure(f(expected), remainder)
+    }
+
 }
+
+typealias Parser<T> = (String) -> Result<T>
+
+fun prefix(value: String): Parser<String> = { input ->
+    if (input.startsWith(value)) {
+        Success(value, input.substring(value.length))
+    } else {
+        Failure(value, input)
+    }
+}
+
+fun integer(input: String): Result<Int> {
+    val match = input.takeWhile { it.isDigit() }
+    return if (match.isNotEmpty()) {
+        Success(match.toInt(), input.substring(match.length))
+    } else {
+        Failure("integer", input)
+    }
+}
+
+fun whitespace(input: String): Result<String> {
+    val match = input.takeWhile { it.isWhitespace() }
+    return if (match.isNotEmpty()) {
+        Success(match, input.substring(match.length))
+    } else {
+        Failure("whitespace", input)
+    }
+}
+
+/*fun <T1, T2> sequence(firstParser: Parser<T1>, secondParser: Parser<T2>): Parser<Pair<T1, T2>> = { input ->
+    when(val firstResult = firstParser(input)) {
+        is Failure -> firstResult
+        is Success -> when (val secondResult = secondParser(firstResult.remainder)) {
+            is Failure -> secondResult
+            is Success -> Success(Pair(firstResult.match, secondResult.match), secondResult.remainder)
+        }
+    }
+}*/
+
+
+/*fun <T1, T2> sequence(firstParser: Parser<T1>, secondParser: Parser<T2>): Parser<Pair<T1, T2>> = { input ->
+    when(val firstResult = firstParser(input)) {
+        is Failure -> firstResult
+        is Success -> secondParser(firstResult.remainder).map { Pair(firstResult.match, it) }
+    }
+}*/
+
+fun <T1, T2> sequence(firstParser: Parser<T1>, secondParser: Parser<T2>): Parser<Pair<T1, T2>> = { input ->
+    firstParser(input)
+        .flatMap { firstResult, remainder -> secondParser(remainder).map { Pair(firstResult, it) } }
+}
+
+infix fun <T1, T2> Parser<T1>.then(otherParser: Parser<T2>): Parser<Pair<T1, T2>> = sequence(this, otherParser)
+
+fun <T> oneOf(firstParser: Parser<T>, secondParser: Parser<T>): Parser<T> = { input ->
+    when (val firstResult = firstParser(input)) {
+        is Success -> firstResult
+        is Failure -> secondParser(input).mapExpected { "${firstResult.expected} or $it" }
+    }
+}
+
+infix fun <T> Parser<T>.or(otherParser: Parser<T>): Parser<T> = oneOf(this, otherParser)
+
+fun <T, U> Parser<T>.map(f: (T) -> U): Parser<U> = { this(it).map(f) }
+
+fun <X, T> Parser<X>.before(p: Parser<T>): Parser<T> = sequence(this, p).map { it.second }
+
+fun <T, Y> Parser<T>.followedBy(y: Parser<Y>): Parser<T> = sequence(this, y).map { it.first  }
--- src/test/kotlin/examples/parsers/ParsersTests.kt
+++ src/test/kotlin/examples/parsers/ParsersTests.kt
@@ -1,16 +1,83 @@
 package examples.parsers
 
+import examples.parsers.Result.Failure
+import examples.parsers.Result.Success
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class ParsersTests : StringSpec({
 
-    "should parse key value pair" {
+    /*"should parse key value pair" {
         keyValuePair("firstName:jan") shouldBe mapOf("firstName" to "jan")
+    }*/
+
+    /*"should parse many key value pairs" {
+        keyValuePair("firstName:jan\nlastName:Nowak") shouldBe mapOf("firstName" to "jan", "lastName" to "Nowak")
+    }*/
+
+/*
+  co jeśli pojawią się kolejne wymagania np:
+      -pary jako wartości
+      -linie z komentarzem
+      -inne separatory
+      -wyrażenia, które trzeba interpretować w specyficzny sposób
+
+   Logika naszego parsera stanie się zawiła i skomplikowana.
+   Jakimś pomysłem mogą wydawać się wyrażenia regularne, ale to zły pomysł :)
+   Źeby rozwiązać problem w efektywny sposób wykorzystamy funkcje i możliwości ich kompozycji
+   Możemy założyć (chociaż to uproszczenie), że parser to prosta funkcja
+   (String) -> Result<T> gdzie Result będzie reprezentowało dwie potencjalne wartości
+   sukces albo porażkę
+*/
+
+    "should parse prefix" {
+        val parser = prefix("-")
+        parser("-jan") shouldBe Success("-", "jan")
+        parser("jan") shouldBe Failure("-", "jan")
     }
 
-    "should parse many key value pairs" {
-        keyValuePair("firstName:jan\nlastName:Nowak") shouldBe mapOf("firstName" to "jan", "lastName" to "Nowak")
+    "should parse integer" {
+        integer("123jan") shouldBe Success(123, "jan")
+        integer("jan") shouldBe Failure("integer", "jan")
+    }
+
+    "should parse whitespace" {
+        whitespace("  jan") shouldBe Success("  ", "jan")
+        whitespace("jan") shouldBe Failure("whitespace", "jan")
+    }
+
+    "should parse sequence" {
+        val parser = sequence(prefix("-"), ::integer)
+        parser("-123") shouldBe Success(Pair("-", 123), "")
+        parser("123") shouldBe Failure("-", "123")
+        val prefixAndInteger = prefix("-").then(::integer)
+        prefixAndInteger("-123") shouldBe Success(Pair("-", 123), "")
+    }
+
+    "should parse one of" {
+        val parser = oneOf(prefix("a"), prefix("b"))
+        parser("ab") shouldBe Success("a", "b")
+        parser("bc") shouldBe Success("b", "c")
+        parser("cd") shouldBe Failure("a or b", "cd")
+        val aOrB = prefix("a").or(prefix("b"))
+        aOrB("ab") shouldBe Success("a", "b")
+    }
+
+    "should map" {
+        integer("11").map { it % 2 == 0 } shouldBe Success(false, "")
+    }
+
+    "should skip first parser" {
+        val parser = ::integer.before(prefix("a"))
+        parser("1a") shouldBe Success("a", "")
+    }
+
+    "should skip following parser" {
+        val parser = prefix("a").followedBy(::integer)
+        parser("a1") shouldBe Success("a", "")
     }
 
 })
+
+
+
